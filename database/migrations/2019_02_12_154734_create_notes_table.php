41a7758b6d03d8c51ebea4758321148310e18e3b
LiviaNascimento <liviapriscilla1989@gmail.com>
2019-02-14 17:47:15 +0000

    Stopped in list notes

--- database/migrations/2019_02_12_154734_create_notes_table.php
+++ database/migrations/2019_02_12_154734_create_notes_table.php
@@ -18,6 +18,8 @@ class CreateNotesTable extends Migration
             $table->string('title');
             $table->text('content');
             $table->datetime('dateNotification');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
             $table->softDeletes(); 
         });
