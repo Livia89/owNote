770a10626f5f471e5cc5ec2ff8a55a13713dc361
LiviaNascimento <liviapriscilla1989@gmail.com>
2019-02-26 12:14:01 +0000

    Implementation notification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -17,9 +18,13 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-
+    
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <!-- ONE Signal -->
+    <link rel="manifest" href="{{ asset('js/onesignal/manifest.json') }}" />
+
     <script>
             window.Laravel = <?php echo json_encode([
                 'csrfToken' => csrf_token(),
@@ -83,7 +88,22 @@
             @yield('content')
         </main>
         <footer>
-        
+        <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+        <script>
+           var OneSignal = window.OneSignal || [];
+           
+            OneSignal.push(function() {
+                OneSignal.init({
+                    
+                appId: "597c7226-83eb-4d80-9fb1-18856e8aaa6f",
+                autoRegister: false,
+                notifyButton: {
+                    enable: true,
+                },
+                allowLocalhostAsSecureOrigin: true
+                });
+            });
+        </script>
         </footer>
     </div>
 </body>
