9e08b05d2721f3d5cee52e931c0c7194b73986fb
Livia89 <liviapriscilla1989@gmail.com>
2020-05-17 16:10:35 +0100

    test

--- app/Note.php
+++ app/Note.php
@@ -5,15 +5,16 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-
+/* teste */
 class Note extends Model
 {
-    use SoftDeletes; /* To delete logical */ 
+    use SoftDeletes; /* To delete logical */
 
     protected $fillable = [
         'title', 'content', 'dateNotification', 'hourNotification', 'user_id'
     ];
 
     protected $dates = ['deleted_at'];
-    
+
+
 }
